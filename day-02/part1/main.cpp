efa976762e1399ec337adf8708642ae2c6492914
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2016-12-02 13:43:01 +0100

    Add day2

---
+++ day-02/part1/main.cpp
@@ -0,0 +1,72 @@
+#include <fstream>
+#include <vector>
+
+std::vector< std::vector<int> > Map = {
+  { 1, 2, 3 },
+  { 4, 5, 6 },
+  { 7, 8, 9 }
+};
+
+struct coordinate {
+  int x;
+  int y;
+};
+
+std::vector<std::string> readfile(std::string filename)
+{
+  std::vector<std::string> full_content;
+  std::string line;
+  std::fstream fin(filename, std::fstream::in);
+
+  while (fin >> line) {
+    full_content.push_back(line);
+  }
+  return full_content;
+}
+
+int main(int argc, const char *argv[])
+{
+  if (argc < 2) {
+    printf("%s <filename>\n", argv[0]);
+    exit(0);
+  }
+  std::vector<std::string> lines = readfile(argv[1]);
+
+  coordinate position = {1, 1}; // start from 5
+
+  for(
+      std::vector<std::string>::iterator line = lines.begin();
+      line != lines.end();
+      ++line
+  ) {
+    for(char& c : *line) {
+      int move_x = 0, move_y = 0;
+      switch(c) {
+        case 'U':
+          move_y = -1;
+          break;
+        case 'L':
+          move_x = -1;
+          break;
+        case 'D':
+          move_y = 1;
+          break;
+        case 'R':
+          move_x = 1;
+          break;
+      }
+
+      if (position.y + move_y < 0) { move_y = 0; }
+      if (position.y + move_y > Map.size() - 1) { move_y = 0; }
+
+      if (position.x + move_x < 0) { move_x = 0; }
+      if (position.x + move_x > Map[position.y].size() - 1) { move_x = 0; }
+
+      position.x += move_x;
+      position.y += move_y;
+    }
+    printf("%d", Map[position.y][position.x]);
+  }
+
+  return 0;
+}
