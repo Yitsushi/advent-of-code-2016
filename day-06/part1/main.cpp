b4b1115e38db909ea5f7e0a1082d5a6828d8afe0
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2016-12-06 15:16:19 +0100

    Add day6

---
+++ day-06/part1/main.cpp
@@ -0,0 +1,65 @@
+#include <vector>
+#include <string>
+#include <fstream>
+
+std::vector<std::string> readfile(std::string filename)
+{
+  std::vector<std::string> sequences;
+  std::string line;
+
+  std::fstream fin(filename, std::fstream::in);
+  while (fin >> line) {
+    for (int i = 0; i < line.length(); ++i) {
+      if (sequences.size() <= i) {
+        sequences.push_back("");
+      }
+      sequences[i] += (char)line[i];
+    }
+  }
+  return sequences;
+}
+
+char mostFrequentCharacter(const std::string str) {
+  int freq[26];
+  // fill with zeros :D to be sure
+  for (int i = 0; i < 26; ++i) {
+    freq[i] = 0;
+  }
+
+  // Count them all
+  for (int i = 0; i < str.length(); ++i) {
+    freq[((char)str[i]) - 'a']++;
+  }
+
+  char most_frequent = 'a';
+  int max = 0;
+  for(int i = 0; i < 26; ++i) {
+    if (freq[i] > max) {
+      max = freq[i];
+      most_frequent = (i + 'a');
+    }
+  }
+
+  return most_frequent;
+}
+
+int main(int argc, const char *argv[])
+{
+  if (argc < 2) {
+    printf("%s <input_file>\n", argv[0]);
+    return 1;
+  }
+
+  std::vector<std::string> messages = readfile(argv[1]);
+
+  for (
+      std::vector<std::string>::iterator message = messages.begin();
+      message != messages.end();
+      ++message
+  ) {
+    printf("%c", mostFrequentCharacter(*message));
+  }
+  printf("\n");
+
+  return 0;
+}
