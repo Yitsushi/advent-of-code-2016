120b535cd8251e1cbba393403ecfb21cd1124b85
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2016-12-16 11:42:03 +0100

    add day-16

---
+++ day-16/part1/main.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <algorithm>
+
+#define INITIAL_SATE "01111010110010011"
+#define DISK_LENGTH 272
+
+#define swap10(ch) ch = (ch == '0' ? '1' : '0')
+#define match_pair(str, i) ((str[i*2] == str[(i*2)+1]) ? '1' : '0')
+
+std::string dragon(std::string str)
+{
+  int len = str.length();
+  for (int i = 0; i < len / 2; ++i) {
+    char first = str[i];
+    char second = str[len - 1 - i];
+
+    swap10(first);
+    swap10(second);
+
+    str[i] = second;
+    str[len - 1 - i] = first;
+  }
+  if (len % 2 == 1) swap10(str[len / 2]);
+
+  return str;
+}
+
+std::string checksum(std::string str)
+{
+  int len = str.length();
+  std::string sum = "";
+  for (int i = 0; i < len / 2; ++i) sum += match_pair(str, i);
+
+  if (sum.length() % 2 == 0) return checksum(sum);
+
+  return sum;
+}
+
+int main()
+{
+  std::string disk_data = INITIAL_SATE;
+  std::string sum = "";
+
+  while (disk_data.length() < DISK_LENGTH) {
+    disk_data += '0' + dragon(disk_data);
+  }
+  disk_data = disk_data.substr(0, DISK_LENGTH);
+  printf("Disk data: %s\n", disk_data.c_str());
+  sum = checksum(disk_data);
+  printf("Checksum:  %s\n", sum.c_str());
+  return 0;
+}
