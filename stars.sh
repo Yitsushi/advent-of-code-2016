b97def87dfbb4547fa9b5316d9852824e1977ace
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2016-12-25 16:01:28 +0100

    Add stars stat from size

--- stars.sh
+++ stars.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-find . -name "main.cpp" -print | \
-  xargs -n1 dirname | \
-  xargs -n1 dirname | \
-  xargs -n1 basename | \
-  uniq -c
+curl -s http://adventofcode.com/2016/stats | \
+  grep stats-both | \
+  sed -Ee 's/.*<a[^>]*> *([0-9]+) *<s[^>]*> *([0-9]+)<\/span> *<s[^>]*> *([0-9]+).*/\1 \2 \3/g' | \
+  awk '
+    BEGIN { hard_day=0; hard_percentage=100; }
+    END {
+      printf("\nThe hardest day was Day-%d, only %.2f%% acquired 2 stars.\n", hard_day, hard_percentage)
+    }
+    {
+      percentage = (1-$3/$2)*100;
+      printf("%d: %d/%d => %.2f%% finished both\n", $1, $3, $2, percentage);
+      if (hard_percentage > percentage) { hard_percentage=percentage; hard_day=$1; }
+    }'
